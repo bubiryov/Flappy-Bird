75181d951e258d53efbeb3dce45eab8b688cc16f
egorbubiryov <egorbubiryov@imakebetter.com>
2024-02-15 11:34:00 +0200

    Initial Commit

---
+++ Flappy Bird/Flappy_BirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flappy_BirdApp.swift
+//  Flappy Bird
+//
+//  Created by Egor Bubiryov on 15.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Flappy_BirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
